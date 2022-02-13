18b3c4c5043f1dbd646495518998b6a69b3ed380
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2022-02-13 19:28:30 +0500

    fonts

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,24 @@ functions.isWebp();
 // import Swiper, { Navigation, Pagination } from 'swiper';
 
 // const swiper = new Swiper();
+
+// Header Menu
+const headerButton = document.querySelector(".header__button");
+const headerMenu = document.querySelector(".header__menu");
+const header = document.querySelector(".header");
+let menuOpened = false;
+const menuToggle = () => { 
+    menuOpened = !menuOpened;
+    headerButton.classList.toggle("open"); 
+    header.classList.toggle("menu-open");
+    headerMenu.classList.toggle("open");
+};
+
+
+headerButton.onclick = menuToggle;
+
+window.onclick = (e) => {
+  if ( menuOpened && !e.composedPath().includes(headerButton) && !e.composedPath().includes(headerMenu) ){
+    menuToggle();
+  }
+};
