aa1765dedc3c7036ea1715b65b963cd092d26506
shahrizoda <shahrizodadeveloping@gmail.com>
2022-02-19 19:55:52 +0500

    contacts block

--- src/js/app.js
+++ src/js/app.js
@@ -454,3 +454,21 @@ document.querySelectorAll('.video-container').forEach(function(el){
   });
 }) 
 
+document.querySelectorAll('.contacts').forEach(function(contact){
+  const buttons = contact.querySelectorAll('.contacts__btn');
+  const info = contact.querySelectorAll('.contacts__info');
+  buttons.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+      buttons.forEach(function(el){
+        el.classList.remove('active');
+      })
+      e.target.classList.add('active');
+      const target = e.target.getAttribute('data-target');
+      const content = contact.querySelector(`#${target}`)
+      info.forEach(function(el){
+        el.classList.remove('active');
+      })
+      content.classList.add('active');
+    })
+  })
+})
